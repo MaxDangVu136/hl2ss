8e609d7a0111720c9df07fc2a63ed708458989d1
Max Dang Vu <mdan066@aucklanduni.ac.nz>
2023-09-01 09:54:02 +1200

    Verifying depth to model transformations. Will transform only to board space in future.

--- viewer/realsense-acquire.py
+++ viewer/realsense-acquire.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 import cv2
+import open3d as o3d
 import ChArUco_pose_est
 
 
@@ -59,40 +60,19 @@ def construct_extrinsic(extrinsic_frame):
     return T_mtx
 
 
-def write_to_csv(filepath, data):
-    """
-    This function writes a 2D array of data points to a CSV file located at a specified filepath.
-
-    Parameters
-    ----------
-    filepath: str
-        path to where the .csv data file is saved.
-
-    data: array
-        k x 3 array of nodal coordinates of data points.
-    """
-
-    with open(filepath, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(np.asarray(data))
-
-
 #   initialise pipeline
 pipeline = rs.pipeline()
 conf = rs.config()
 
-depth = True
-rgb_ir_imu = False
+depth = False
+rgb_ir_imu = True
 
-if depth:
-    conf.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
-    conf.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
-
-else:
-    conf.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
-    conf.enable_stream(rs.stream.infrared, 1, 1280, 720, rs.format.y8, 30)
-
+# conf.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+conf.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
+conf.enable_stream(rs.stream.infrared, 1, 1280, 720, rs.format.y8, 30)
 conf.enable_stream(rs.stream.accel)
+
+align = rs.align(rs.stream.color)
 
 #   start streaming
 pipeline.start(conf)
@@ -105,39 +85,45 @@ try:
         if depth:
             #  collect data frames
             depth_frame = f.get_depth_frame()
+            color_frame = f.get_color_frame()
             accel_frame = f.first_or_default(rs.stream.accel)
+            aligned_frames = align.process(f)
+            aligned_depth_frame = aligned_frames.get_depth_frame()
+            aligned_color_frame = aligned_frames.get_color_frame()
 
             #   extract sensor intrinsics (https://github.com/IntelRealSense/librealsense/issues/10180)
             depth_frame_intrinsic = depth_frame.profile.as_video_stream_profile().get_intrinsics()
             depth_intrinsic, depth_distortion = construct_intrinsic(depth_frame_intrinsic)
 
             #   extract transformation matrices from depth and RGB to accelerometer space
+            # depth_to_RGB_extrinsic = depth_frame.profile.get_extrinsics_to(RGB_frame.profile)
+            # T_depth_to_RGB = construct_extrinsic(depth_to_RGB_extrinsic)
             depth_to_accel_extrinsic = depth_frame.profile.get_extrinsics_to(accel_frame.profile)
             T_depth_to_accel = construct_extrinsic(depth_to_accel_extrinsic)
 
-            #   declare pointcloud object, for calculating pointclouds and texture mappings
-            pc = rs.pointcloud()
+            aligned_color_to_depth = aligned_color_frame.profile.get_extrinsics_to(depth_frame.profile)
+            T_aligned_color_depth = construct_extrinsic(aligned_color_to_depth)
 
-            #   generate texture for our PLY
-            colorizer = rs.colorizer()
-            colorized = colorizer.process(f)
-            colorized_depth = colorizer.colorize(depth_frame)
-            depth_colormap = np.asanyarray(colorized_depth.get_data())
+            aligned_depth_to_color = aligned_depth_frame.profile.get_extrinsics_to(color_frame.profile)
+            T_aligned_depth_color = construct_extrinsic(aligned_depth_to_color)
 
-            #   extract point coordinates from depth cloud
-            points = pc.calculate(depth_frame)
-            v, t = points.get_vertices(), points.get_texture_coordinates()
-            verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
-            texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+            aligned_color_to_accel = aligned_color_frame.profile.get_extrinsics_to(accel_frame.profile)
+            T_aligned_color_accel = construct_extrinsic(aligned_color_to_accel)
 
-            #   measuring distance of points from depth data
-            #   https://github.com/IntelRealSense/librealsense/blob/master/examples/C/distance/rs-distance.c
-            #   https://dev.intelrealsense.com/docs/rs-measure
-            #   Note: All stereo-based 3D cameras have the property of noise being proportional to distance squared.
-            # To counteract this we transform the frame into disparity-domain making the noise more uniform across distance
+            aligned_depth_aligned_color = aligned_depth_frame.profile.get_extrinsics_to(aligned_color_frame.profile)
+            T_aligned_dc = construct_extrinsic(aligned_depth_aligned_color)
 
-            #   show data
-            cv2.imshow("Depth", depth_colormap)
+            # depth_to_ir1_extrinsic = depth_frame.profile.get_extrinsics_to(ir1_frame.profile)
+            # T_depth_to_ir1 = construct_extrinsic(depth_to_ir1_extrinsic)
+
+            # #   collect image data if image frames are detected
+            # depth_img = np.asanyarray(depth_frame.get_data())
+
+            #   display aligned rgb image and change color mapping
+            img = np.asarray(aligned_color_frame.get_data())
+            # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            cv2.namedWindow("Video", cv2.WINDOW_GUI_EXPANDED)
+            cv2.imshow("Video", img)
 
         elif rgb_ir_imu:
             RGB_frame = f.get_color_frame()
@@ -179,27 +165,39 @@ finally:
 #%%
 if depth:
     #   save acceleration to depth transformations
-    ChArUco_pose_est.write_to_csv("data/realsense/T_depth_accel.csv", T_depth_to_accel)
+    ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_depth_accel.csv", T_depth_to_accel)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_aligned_color_accel.csv", T_aligned_color_accel)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_aligned_depth_accel.csv", T_aligned_depth_accel)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_depth_ir.csv")
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_depth_RGB.csv", T_depth_to_RGB)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_aligned_dc.csv", T_aligned_dc)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_aligned_color_depth.csv", T_aligned_color_depth)
+    # ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_aligned_depth_color.csv", T_aligned_depth_color)
 
-    #   export depth point cloud as PLY file (https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/export_ply_example.py)
-    ply = rs.save_to_ply("data/realsense/point_cloud.ply")
+    #   declare pointcloud object, for calculating pointclouds and texture mappings
+    pc = rs.pointcloud()
 
-    # Set options to the desired values
-    # In this example we'll generate a textual PLY with normals (mesh is already created by default)
-    ply.set_option(rs.save_to_ply.option_ply_binary, False)
-    ply.set_option(rs.save_to_ply.option_ply_normals, True)
+    #   mapping texture onto point cloud
+    pc.map_to(aligned_color_frame)
 
-    # Apply the processing block to the frameset which contains the depth frame and the texture
-    ply.process(colorized)
+    #   extract point coordinates from depth cloud
+    points = pc.calculate(aligned_depth_frame)
+    texture = np.asarray(aligned_color_frame.get_data())
+    points.export_to_ply("data/realsense/point_cloud_depth.ply", aligned_color_frame)
+    # texture = cv2.cvtColor(texture, cv2.COLOR_RGB2BGR)
 
-    ChArUco_pose_est.create_point_cloud("data/realsense/depth_vertices.ply", verts)
+    #   measuring distance of points from depth data
+    #   https://github.com/IntelRealSense/librealsense/blob/master/examples/C/distance/rs-distance.c
+    #   https://dev.intelrealsense.com/docs/rs-measure
+    #   Note: All stereo-based 3D cameras have the property of noise being proportional to distance squared.
+    # To counteract this we transform the frame into disparity-domain making the noise more uniform across distance
 
 if rgb_ir_imu:
     #   save image data
-    cv2.imwrite('data/realsense/ir1_img.png', ir1_img)
-    cv2.imwrite('data/realsense/RGB_img.png', RGB_img)
-    ChArUco_pose_est.write_to_csv('data/realsense/ir1_img.csv', ir1_img)
-    ChArUco_pose_est.write_to_csv('data/realsense/RGB_img.csv', RGB_img)
+    cv2.imwrite('data/realsense/image_data/ir1_img.png', ir1_img)
+    cv2.imwrite('data/realsense/image_data/RGB_img.png', RGB_img)
+    ChArUco_pose_est.write_to_csv('data/realsense/image_data/ir1_img.csv', ir1_img)
+    ChArUco_pose_est.write_to_csv('data/realsense/image_data/RGB_img.csv', RGB_img)
 
 #   ChArUco board specs (m squares x n squares)
 Board16x12 = {
@@ -209,26 +207,28 @@ Board16x12 = {
     'marker_length': 0.012,  # length of ArUco marker in m
     'num_px_height': 600,  # number of pixels in the x direction
     'num_px_width': 600,  # number of pixels in the y direction
-    'aruco_dict': cv2.aruco.DICT_6X6_100  # ArUco dictionary with 100 different unique marjers, each marker in
+    'aruco_dict': cv2.aruco.DICT_6X6_100  # ArUco dictionary with 100 different unique markers, each marker in
     # dictionary has a distinct pattern. This pattern is defined by a
     # grid of 6x6 black and white cells
 }
+
+#   CONVERT THESE TO FUNCTIONS
 
 #   1. GENERATE CHARUCO BOARD AND THEIR OBJECT POINTS (IN BOARD COORDINATE SYSTEM)
 board_specs = Board16x12
 board, aruco_params, aruco_dictionary = ChArUco_pose_est.generate_charuco_board(specs=board_specs)
 
-#   Generate Charuco board image
-img_size = (800, 600)
-charuco_img = board.draw(img_size)
-
-#   Display the Charuco board
-plt.imshow(charuco_img, cmap='gray')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('ChArUco board')
-plt.show()
-# ----------
+# #   Generate Charuco board image
+# img_size = (800, 600)
+# charuco_img = board.draw(img_size)
+#
+# #   Display the Charuco board
+# plt.imshow(charuco_img, cmap='gray')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('ChArUco board')
+# plt.show()
+# # ----------
 
 #%%
 #   2. DETECT CHARUCO BOARD CORNERS
@@ -259,7 +259,7 @@ T_board_to_RGB = ChArUco_pose_est.charucoboard_to_camspace(rvec=rvecs, tvec=tvec
 ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_board_RGB.csv", T_board_to_RGB)
 rigid_board_points_h = ChArUco_pose_est.rigid_base_corners_on_board(board_spec=board_specs, cube_size=0.030, z=0.0)
 rigid_board_points = rigid_board_points_h[:-1, :].T
-ChArUco_pose_est.write_to_csv("data/realsense/rigid_corners_board.csv", rigid_board_points_h.T)
+ChArUco_pose_est.write_to_csv("data/realsense/simulation_data/rigid_corners_board.csv", rigid_board_points_h.T)
 
 #   Board to colour coordinates
 img_points, img_points_h = ChArUco_pose_est.transform_from_X_to_Y(T_board_to_RGB, rigid_board_points_h)
@@ -276,9 +276,11 @@ cv2.waitKey(0)
 
 # #   Transform gravity vector from accel space to board space via infrared camera space
 T_board_to_RGB = np.vstack((T_board_to_RGB, [0., 0., 0., 1.]))
-ChArUco_pose_est.write_to_csv("data/realsense/T_board_RGB.csv", T_board_to_RGB)
+ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_board_RGB.csv", T_board_to_RGB)
 
 T_accel_to_ir1 = np.vstack((T_accel_to_ir1, [0., 0., 0., 1.]))
+ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_accel_ir1.csv", T_accel_to_ir1)
+
 T_ir1_to_RGB = np.vstack((RGB_intrinsic @ T_ir1_to_RGB, [0., 0., 0., 1.]))
 T_accel_to_RGB = T_ir1_to_RGB @ T_accel_to_ir1
 
@@ -286,12 +288,15 @@ T_ir1_to_board = np.linalg.inv(T_board_to_RGB) @ T_ir1_to_RGB
 T_accel_to_board = T_ir1_to_board @ T_accel_to_ir1
 
 accel_h = np.vstack(([accel, 0.]))
+print("gravity IMU:", accel)
 ChArUco_pose_est.write_to_csv("data/realsense/transformations/T_accel_board.csv", T_accel_to_board)
 ChArUco_pose_est.write_to_csv("data/realsense/transformations/gravity.csv", accel.T)
 
 #   x,y,z is right hand coordinate system, so flip y and z axis directions to match board orientation.
 gravity_accel_board = T_accel_to_board @ accel_h
+print("gravity in board:", gravity_accel_board)
 gravity_unit = gravity_accel_board/np.linalg.norm(gravity_accel_board)
+print("unit gravity:", gravity_unit)
 gravity_info = np.array([gravity_accel_board[0], -gravity_accel_board[1], -gravity_accel_board[2]])
 print("gravity info:", gravity_info.T)
 
@@ -301,12 +306,12 @@ gravity_in_RGB = np.reshape(gravity_in_RGB, (-1, 1))
 print("gravity in RGB:", gravity_in_RGB)
 
 #   Calculate end point of vector to draw on image.
-scale = 0.001
+scale = 0.1
 thickness = 5
 color = (0, 165, 255) # BGR color (red)
 start_point = (overlay_img.shape[1]//2,
                overlay_img.shape[0]//2)
-end_point = (int(overlay_img.shape[1]/2 + gravity_in_RGB[0]*scale),
+end_point = (int(overlay_img.shape[1]/2 - gravity_in_RGB[0]*scale),
              int(overlay_img.shape[0]/2 + gravity_in_RGB[1]*scale))
 
 [cv2.circle(overlay_img, tuple(img_points[idx, :].astype(int)), radius=10,
--- viewer/realsense_combine_info.py
+++ viewer/realsense_combine_info.py
@@ -3,108 +3,160 @@ import pandas as pd
 import open3d as o3d
 import ChArUco_pose_est
 
+def pc_correct_orient(file_in, file_out, T_fix):
+    """
+
+    :param file_in:
+    :param file_out:
+    :param T_fix:
+    :return:
+    """
+    #   Invert y and z axes of point cloud
+    pcd = o3d.io.read_point_cloud(file_in)
+    verts = np.asarray(pcd.points)
+
+    #   Apply transformation to correct point cloud orientation
+    verts_h = ChArUco_pose_est.homogeneous_vectors(verts, 'column').T
+    verts_transformed, _ = ChArUco_pose_est.transform_from_X_to_Y(T_fix, verts_h)
+
+    #   Save updated point cloud
+    pcd.points = o3d.utility.Vector3dVector(verts_transformed)
+    o3d.io.write_point_cloud(file_out, pcd)
+
+
+def point_cloud_points(file):
+    """
+
+    :param file:
+    :return:
+    """
+    pc = o3d.io.read_point_cloud(file)
+    points = np.asarray(pc.points)
+
+    return points, pc
+
+
+#   Save realsense point cloud to correct orientation
+T_fix_pc = np.array([[1., 0., 0., 0.],
+                     [0., -1., 0., 0.],
+                     [0., 0., -1., 0.],
+                     [0., 0., 0., 1.]])
+
+data = np.loadtxt("data/cantilever/deformed_model_c1_9.2.txt")
+ChArUco_pose_est.create_point_cloud("data/cantilever/deformed_model_c1_9.2.ply", data)
+
+pc_correct_orient('data/realsense/point_cloud_depth.ply',
+                  'data/realsense/point_cloud_depth_modified.ply',
+                  T_fix_pc)
 
 #   Rigid base corners (board b)
 rigid_corners_board = pd.read_csv('data/realsense/simulation_data/rigid_corners_board.csv',
                                   sep=',', header=None).values.T
 
 #   Gravity vector (accelerometer a)
-gravity = pd.read_csv('data/realsense/simulation_data/gravity.csv',
+gravity = pd.read_csv('data/realsense/transformations/gravity.csv',
                       sep=',', header=None).values.reshape(-1, 1)
 gravity_unit = gravity/np.linalg.norm(gravity)
 gravity_h = np.vstack((gravity, [0.]))
 
 #   Depth point cloud (depth d)
-pc = o3d.io.read_point_cloud('data/realsense/simulation_data/depth_vertices.ply')
-depth_points = np.asarray(pc.points)
+depth_points, depth_pc = point_cloud_points('data/realsense/point_cloud_depth_modified.ply')
 depth_points_h = ChArUco_pose_est.homogeneous_vectors(depth_points, 'column').T
 
-#   Mechanics model (undeformed and deformed)
-model_nodes = pd.read_csv(
-    'data/cantilever/model_nodes.csv', sep=',', header=None).values
-model_corner_nodes = pd.read_csv(
-    'data/cantilever/model_fixed_end_corners.csv', sep=',', header=None).values
-deformed_nodes = pd.read_csv(
-    'data/cantilever/deformed_model_nodes.csv', sep=',', header=None).values
+# #   Mechanics model (undeformed and deformed)
+# model_nodes = pd.read_csv(
+#     'data/cantilever/model_nodes.csv', sep=',', header=None).values
+# model_corner_nodes = pd.read_csv(
+#     'data/cantilever/model_fixed_end_corners.csv', sep=',', header=None).values
+# deformed_nodes = pd.read_csv(
+#     'data/cantilever/deformed_model_nodes.csv', sep=',', header=None).values
+#
+# undeformed_model_nodes = ChArUco_pose_est.reorder_model_node_coordinates(model_nodes, False)
+# undeformed_model_nodes_h = ChArUco_pose_est.homogeneous_vectors(undeformed_model_nodes, 'column').T
+#
+# deformed_model_nodes = ChArUco_pose_est.reorder_model_node_coordinates(deformed_nodes, False)
+# deformed_model_nodes_h = ChArUco_pose_est.homogeneous_vectors(deformed_model_nodes, 'column').T
 
-undeformed_model_nodes = ChArUco_pose_est.reorder_model_node_coordinates(model_nodes, False)
-undeformed_model_nodes_h = ChArUco_pose_est.homogeneous_vectors(undeformed_model_nodes, 'column').T
-
-deformed_model_nodes = ChArUco_pose_est.reorder_model_node_coordinates(deformed_nodes, False)
-deformed_model_nodes_h = ChArUco_pose_est.homogeneous_vectors(deformed_model_nodes, 'column').T
-
-#   Transformations required
+#   Transformations from RealSense
 T_depth_accel_ext = pd.read_csv('data/realsense/transformations/T_depth_accel.csv',
                             sep=',', header=None).values
 T_depth_accel = np.vstack((T_depth_accel_ext, [0., 0., 0., 1.]))
 T_accel_board = pd.read_csv('data/realsense/transformations/T_accel_board.csv',
                             sep=',', header=None).values
+# T_aligned_color_depth = pd.read_csv('data/realsense/transformations/T_aligned_color_depth.csv',
+#                             sep=',', header=None).values
+# T_aligned_color_depth = np.vstack((T_aligned_color_depth, [0., 0., 0., 1.]))
 
-T_deformed_undeformed = np.array([[-0.01, 0.79, -0.61, 0.015],
-                                  [1.00, 0.01, 0.00, 0.015],
-                                  [0.01, -0.61, -0.79, 0.00],
-                                  [0., 0., 0., 1.]])
-
-#   Note that 'model' here is the undeformed model.
-model_corner_nodes = ChArUco_pose_est.reorder_model_node_coordinates(
-    model_corner_nodes, True)
-ChArUco_pose_est.create_point_cloud(
-    'data/realsense/simulation_data/undeformed_model_corners.ply', model_corner_nodes)
+# #   Note that 'model' here is the undeformed model.
+# model_corner_nodes = ChArUco_pose_est.reorder_model_node_coordinates(
+#     model_corner_nodes, True)
+# deformed_model_corners, _ = point_cloud_points('data/realsense/simulation_data/deformed_model_corners.ply')
+#
+# deformed_model_corners = np.array([deformed_model_corners[3,:],
+#                             deformed_model_corners[2,:],
+#                             deformed_model_corners[0,:],
+#                             deformed_model_corners[1,:]])
+#
+# #   Transform deformed model nodes to undeformed model nodes via base corners
+# R, t = ChArUco_pose_est.find_transformation(model_corner_nodes, deformed_model_corners)
+# T_deformed_undeformed = np.vstack((np.hstack((R, t.reshape(-1, 1))), [0., 0., 0., 1.]))
 
 #   Compute transformation between board and model spaces
 rigid_corners_board_align = rigid_corners_board.T[:, :-1]
-rotation, translation = ChArUco_pose_est.find_transformation(
-    rigid_corners_board_align, model_corner_nodes)
-T_model_board = np.vstack(
-    (np.hstack((rotation, translation.reshape(-1, 1))), [0., 0., 0., 1.]))
-T_board_model = np.linalg.inv(T_model_board)
+ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/rigid_base_corners.ply",
+                                    rigid_corners_board_align)
+# rotation, translation = ChArUco_pose_est.find_transformation(
+#     model_corner_nodes, rigid_corners_board_align)
+# T_board_model = np.vstack(
+#     (np.hstack((rotation, translation.reshape(-1, 1))), [0., 0., 0., 1.]))
 
 #   Inferred transformations
-T_accel_model = T_board_model @ T_accel_board
-T_depth_model = T_accel_model @ T_depth_accel
+# T_accel_model = T_board_model @ T_accel_board
+# T_depth_model = T_accel_model @ T_depth_accel
+T_depth_board = T_accel_board @ T_depth_accel
+T_depth_board[0,-1] = T_depth_board[0,-1] - 0.016
 
 #   Transform depth, accel and board points to model space
-depth_in_model, depth_in_model_h = ChArUco_pose_est.transform_from_X_to_Y(
-    T_depth_model, depth_points_h)
-_, accel_in_model_h = ChArUco_pose_est.transform_from_X_to_Y(
-    T_accel_model, gravity_h)
-board_corners_in_model, board_corner_in_model_h = ChArUco_pose_est.transform_from_X_to_Y(
-    T_board_model, rigid_corners_board)
-deformed_in_undeformed, deformed_in_undeformed_h = ChArUco_pose_est.transform_from_X_to_Y(
-        T_deformed_undeformed, ChArUco_pose_est.homogeneous_vectors(deformed_model_nodes, 'column').T)
-pc.points = o3d.utility.Vector3dVector(depth_in_model*1000)
+#   NOTE: do I want to do this, we are adding errors to our source data??
+# depth_in_model, depth_in_model_h = ChArUco_pose_est.transform_from_X_to_Y(
+#     T_depth_model, depth_points_h)
 
-# Bounding box definitions
-x_min, x_max = -1/1000, 31/1000
-y_min, y_max = depth_points[:,1].min(axis=0), 31/1000
-# z_min, z_max = 0/1000, depth_points[:,2].min(axis=0)
+# aligned_color_in_model, _ = ChArUco_pose_est.transform_from_X_to_Y(
+#     T_aligned_color_depth_model, depth_in_model_h)
 
-# Keep only points inside the bounding box
-filtered_pc = np.array([pt for pt in depth_in_model if x_min <= pt[0] <= x_max and y_min <= pt[1] <= y_max])
+depth_in_board, depth_in_board_h = ChArUco_pose_est.transform_from_X_to_Y(
+    T_depth_board, depth_points_h)
+_, accel_in_board_h = ChArUco_pose_est.transform_from_X_to_Y(
+    T_accel_board, gravity_h)
+gravity_corrected = np.array([accel_in_board_h[0], -accel_in_board_h[1], -accel_in_board_h[2]])
+
+# board_corners_in_model, board_corner_in_model_h = ChArUco_pose_est.transform_from_X_to_Y(
+#     T_board_model, rigid_corners_board)
+# deformed_in_undeformed, deformed_in_undeformed_h = ChArUco_pose_est.transform_from_X_to_Y(
+#         T_deformed_undeformed, ChArUco_pose_est.homogeneous_vectors(deformed_model_nodes, 'column').T)
+
+# filtered_pc, _ = point_cloud_points('data/realsense/simulation_data/filtered_depth_point_cloud_right.ply')
+# ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/horizontal_beam_right.txt',
+#                               filtered_pc/1000, ' ')
 
 #   Export info and view in meshlab
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/cantilever_model.txt',
-                              1000 * undeformed_model_nodes, ',')
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/depth_data.txt',
-                              1000 * depth_in_model, ',')
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/rigid_base_corners.txt',
-                              1000 * board_corners_in_model, ',')
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/deformed_model.txt',
-                              1000 * deformed_in_undeformed, ',')
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/transformed_gravity.txt',
-                              accel_in_model_h[:-1], ',')
-ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/gravity.txt',
-                              gravity, ',')
+# ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/cantilever_model.txt',
+#                               1000 * undeformed_model_nodes, ' ')
+# ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/rigid_base_corners.txt',
+#                               1000 * board_corners_in_model, ' ')
+# ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/deformed_model.txt',
+#                               1000 * deformed_in_undeformed, ' ')
+ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/gravity_board_corrected.txt',
+                              gravity_corrected.T, ' ')
+ChArUco_pose_est.write_to_txt('data/realsense/simulation_data/depth_vertices.txt',
+                              depth_in_board, ' ')
 
-ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/cantilever_model.ply",
-                                    undeformed_model_nodes * 1000)
-o3d.io.write_point_cloud("data/realsense/simulation_data/transformed_depth_points.ply",
-                         pc, write_ascii=True)
-ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/transformed_filtered_depth_points.ply",
-                                    filtered_pc * 1000)
-ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/transformed_rigid_corner.ply",
-                                    board_corners_in_model * 1000)
-ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/transformed_deformed_geometry.ply",
-                                    deformed_in_undeformed * 1000)
+depth_pc.points = o3d.utility.Vector3dVector(depth_in_board)
+o3d.io.write_point_cloud("data/realsense/simulation_data/depth_in_board.ply",
+                         depth_pc, write_ascii=True)
+# ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/transformed_rigid_corner.ply",
+#                                     board_corners_in_model * 1000)
+# ChArUco_pose_est.create_point_cloud("data/realsense/simulation_data/transformed_deformed_geometry.ply",
+#                                     deformed_in_undeformed * 1000)
 
 print("DONE!")
